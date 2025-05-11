50dcfd08c960f6bb69351cc0134607fec046ce32
Muhire James <mujames008@gmail.com>
2025-05-11 12:06:19 +0200

    fixed proxy error

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5100/api",
+  baseURL: "http://localhost:5100/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
